38247cf70ec6fa0ad830d3f23de8d5211f62efd6
sori14 <sori.juggling@gmail.com>
2020-05-06 17:44:31 +0900

    Modify unicorn settings version 2.0

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,13 +1,13 @@
 # ワーカーの数
-$worker  = 2
+$worker = 2
 # 何秒経過すればワーカーを削除するのかを決める
 $timeout = 30
 # 自分のアプリケーション名、currentがつくことに注意
 $app_dir = "/var/www/rails/sns_app/current"
 # リクエストを受け取るポート番号を指定
-$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$listen = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 # PIDの管理ファイルディレクトリ
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 # エラーログを吐き出すファイルのディレクトリ
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
@@ -17,7 +17,7 @@ working_directory $app_dir
 stderr_path $std_log
 stdout_path $std_log
 timeout $timeout
-listen  $listen
+listen $listen
 pid $pid
 
 # ホットデプロイをするかしないかを設定
